9d98fb96ad2a8d0d252e4f8185249b7bee8f8810
shichang.wang <shichang.wang>
2019-07-12 13:09:11 +0800

    no message

--- setup/MessageBank.rb
+++ setup/MessageBank.rb
@@ -35,20 +35,11 @@ module Pod
       end
 
       puts "\n------------------------------"
-      puts ""
-      puts "To get you started we need to ask a few questions, this should only take a minute."
-      puts ""
+      
 
       has_run_before = `defaults read org.cocoapods.pod-template HasRunBefore`.chomp == "1"
 
-      puts "If this is your first time we recommend running through with the guide: "
-      puts " - "  + "https://guides.cocoapods.org/making/using-pod-lib-create.html".blue.underlined
-
-      if ENV["TERM_PROGRAM"] == "iTerm.app"
-        puts " ( hold cmd and click links to open in a browser. )".magenta
-      else
-        puts " ( hold cmd and double click links to open in a browser. )".magenta
-      end
+      
 
       unless has_run_before
         puts "\n Press return to continue."
